7bf4d1fed7aec81ad2c340e5ddc0df4daabc083b
smlnatya@gmail.com <smlnatya@gmail.com>
2020-03-06 21:22:45 -0500

    Added Theme to the project

--- SchoolManagement/App_Start/BundleConfig.cs
+++ SchoolManagement/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace SchoolManagement
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/Flatly.css",
                       "~/Content/site.css"));
         }
     }
